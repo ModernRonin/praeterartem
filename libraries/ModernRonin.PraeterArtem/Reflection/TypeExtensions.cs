8712d4274855f254396ead2ed852b1e8e1626d89
Max <amodernronin@gmail.com>
2014-05-10 22:38:05 +0200

    * added .CSharpName() and tests * fixed a few comments

--- libraries/ModernRonin.PraeterArtem/Reflection/TypeExtensions.cs
+++ libraries/ModernRonin.PraeterArtem/Reflection/TypeExtensions.cs
@@ -1,34 +1,83 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
 
 namespace ModernRonin.PraeterArtem.Reflection
 {
-	/// <summary>
-	/// Provides extension methods for the <see cref="Type"/> class.
-	/// </summary>
-	public static class TypeExtensions
-	{
-		/// <summary>Indicates whether one or more attributes of the specified type or of its derived types is applied to this member.</summary>
-		/// <typeparam name="TAttribute">The attribute to look for.</typeparam>
-		/// <returns>true if a custom attribute of type attributeType is applied to element; otherwise, false.</returns>
-		public static bool HasAttribute<TAttribute>([NotNull] this MemberInfo type)
-			where TAttribute : Attribute
-		{
-			if (type == null) throw new ArgumentNullException("type");
-			return Attribute.IsDefined(type, typeof (TAttribute));
-		}
-
-		/// <summary> Indicates whether there are public methods declared on the type (and not base type)</summary>
-		/// <returns>true if any method is located in the specified type; otherwise, false</returns>
-		public static bool HasPublicMethods([NotNull] this Type type)
-		{
-			if (type == null) throw new ArgumentNullException("type");
-			return type
-				.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
-				.Where(m => !m.IsSpecialName)
-				.Any(m => m.DeclaringType == type);
-		}
-	}
+    /// <summary>
+    ///     Just a bunch of extensions for <see cref="Type" /> which MS has forgotten.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        static readonly Dictionary<Type, string> sCSharpTypeAliases =
+            new Dictionary<Type, string>
+            {
+                {typeof (string), "string"},
+                {typeof (void), "void"},
+                {typeof (object), "object"},
+                {typeof (bool), "bool"},
+                {typeof (char), "char"},
+                {typeof (int), "int"},
+                {typeof (uint), "uint"},
+                {typeof (long), "long"},
+                {typeof (ulong), "ulong"},
+                {typeof (short), "short"},
+                {typeof (ushort), "ushort"},
+                {typeof (byte), "byte"},
+                {typeof (sbyte), "sbyte"},
+                {typeof (float), "float"},
+                {typeof (double), "double"},
+            };
+        /// <summary>Check whether an attribute is set for this member.</summary>
+        /// <remarks>Multiple attributes count as true, too. Also, attributes derived from <typeparamref name="T" /> count, too.s</remarks>
+        /// <returns>whether a custom attribute of member <typeparamref name="T" /> is set on <paramref name="member" /></returns>
+        public static bool HasAttribute<T>([NotNull] this MemberInfo member)
+            where T : Attribute
+        {
+            if (member == null)
+                throw new ArgumentNullException("member");
+            return Attribute.IsDefined(member, typeof (T));
+        }
+        /// <summary>
+        ///     Indicates whether there are any public methods declared for this type, excluding methods defined in base
+        ///     classes.
+        /// </summary>
+        public static bool HasPublicMethods([NotNull] this Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+            return
+                type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
+                                BindingFlags.Static)
+                    .Where(m => !m.IsSpecialName)
+                    .Any(m => m.DeclaringType == type);
+        }
+        /// <summary>
+        ///     Gets a pretty name for a type even if it is generic. For example, for Dictionary{string,object} it will return
+        ///     exactly that instead of
+        ///     "Dictionary'1[System.String...] as type.Name would do.
+        /// </summary>
+        public static string PrettyName(this Type type)
+        {
+            return type.IsGenericType
+                ? String.Format("{0}<{1}>", type.Name.Split('`')[0],
+                    String.Join(", ",
+                        type.GetGenericArguments()
+                            .Select(PrettyName)
+                            .ToArray()))
+                : type.Name;
+        }
+        /// <summary>
+        ///     Returns the C# type name instead of the CLR type name for those types which have a special name in C#, for example
+        ///     "int" instead of Int32 etc.
+        /// </summary>
+        public static string CSharpName(this Type type)
+        {
+            if (sCSharpTypeAliases.ContainsKey(type))
+                return sCSharpTypeAliases[type];
+            return type.Name;
+        }
+    }
 }
--- tests/ModernRonin.PraeterArtem.UnitTests/Reflection/TypeExtensionsTests.cs
+++ tests/ModernRonin.PraeterArtem.UnitTests/Reflection/TypeExtensionsTests.cs
@@ -1,51 +1,89 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Text;
+using FluentAssertions;
 using FluentAssertions.Common;
 using JetBrains.Annotations;
 using ModernRonin.PraeterArtem.Annotations;
 using Xunit;
+using Xunit.Extensions;
 using TypeExtensions = ModernRonin.PraeterArtem.Reflection.TypeExtensions;
 
 namespace ModernRonin.PraeterArtem.UnitTests.Reflection
 {
-	public sealed class TypeExtensionsTests
-	{
-		[Fact]
-		public void HasAttribute_When_There_AreNo_Attributes_Should_Return_False()
-		{
-			typeof (TypeExtensionsTests)
-				.HasAttribute<DataTransferObjectAttribute>()
-				.Should().BeFalse();
-		}
-		[Fact]
-		public void HasAttribute_When_There_Is_Attribute_Should_Return_True()
-		{
-			typeof (WithPublicMethods)
-				.HasAttribute<UsedImplicitlyAttribute>()
-				.Should().BeTrue();
-		}
+    public sealed class TypeExtensionsTests
+    {
+        [Fact]
+        public void HasAttribute_When_There_AreNo_Attributes_Returns_False()
+        {
+            typeof (TypeExtensionsTests)
+                .HasAttribute<DataTransferObjectAttribute>()
+                .Should()
+                .BeFalse();
+        }
+        [Fact]
+        public void HasAttribute_When_There_Is_Attribute_Returns_True()
+        {
+            typeof (WithPublicMethods).HasAttribute<UsedImplicitlyAttribute>()
+                                      .Should()
+                                      .BeTrue();
+        }
+        [Fact]
+        public void
+            HasPublicMethods_When_There_Are_Public_Methods_Returns_True()
+        {
+            TypeExtensions.HasPublicMethods(typeof (WithPublicMethods))
+                          .Should()
+                          .BeTrue();
+        }
+        [Fact]
+        public void
+            HasPublicMethods_When_There_AreNo_Public_Methods_Returns_False()
+        {
+            TypeExtensions.HasPublicMethods(typeof (WithoutPublicMethods))
+                          .Should()
+                          .BeFalse();
+        }
+        [Theory]
+        [InlineData(typeof(string), "string")]
+        [InlineData(typeof(void), "void")]
+        [InlineData(typeof(object), "object")]
+        [InlineData(typeof(bool), "bool")]
+        [InlineData(typeof(char), "char")]
+        [InlineData(typeof(int), "int")]
+        [InlineData(typeof(uint), "uint")]
+        [InlineData(typeof(long), "long")]
+        [InlineData(typeof(ulong), "ulong")]
+        [InlineData(typeof(short), "short")]
+        [InlineData(typeof(ushort), "ushort")]
+        [InlineData(typeof(byte), "byte")]
+        [InlineData(typeof(sbyte), "sbyte")]
+        [InlineData(typeof(float), "float")]
+        [InlineData(typeof(double), "double")]
+        public void GetCSharpTypeName_SpecialTypes(Type type, string expected)
+        {
+            TypeExtensions.CSharpName(type).Should().Be(expected);
+        }
+        [Fact]
+        public void GetCSharpTypeName_OtherType()
+        {
+            TypeExtensions.CSharpName(typeof(StringBuilder))
+                              .Should()
+                              .Be("StringBuilder");
+        }
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers /* By reflection*/
+            )]
+        abstract class WithPublicMethods
+        {
+            public void A() {}
+            public static void B() {}
+        }
 
-		[Fact]
-		public void HasPublicMethods_When_There_Are_Public_Methods_Should_Return_True()
-		{
-			TypeExtensions.HasPublicMethods(typeof(WithPublicMethods)).Should().BeTrue();
-		}
-
-		[Fact]
-		public void HasPublicMethods_When_There_AreNo_Public_Methods_Should_Return_False()
-		{
-			TypeExtensions.HasPublicMethods(typeof (WithoutPublicMethods)).Should().BeFalse();
-		}
-		[UsedImplicitly(ImplicitUseTargetFlags.WithMembers /* By reflection*/)]
-		abstract class WithPublicMethods
-		{
-			public void A() { }
-			public static void B() { }
-		}
-		[UsedImplicitly(ImplicitUseTargetFlags.WithMembers /* By reflection*/)]
-		sealed class WithoutPublicMethods : WithPublicMethods
-		{
-			private void X() { }
-			private static void Y() { }
-		}
-	}
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers /* By reflection*/
+            )]
+        sealed class WithoutPublicMethods : WithPublicMethods
+        {
+            void X() {}
+            static void Y() {}
+        }
+    }
 }
