344e32e88ea72d37014c6edd8e5122bf512ce13d
Max <amodernronin@gmail.com>
2014-05-14 15:51:48 +0200

    listing loaded assemblies in domain now

--- prototypes/DomainLoader/Program.cs
+++ prototypes/DomainLoader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.Linq;
+using ModernRonin.PraeterArtem.Functional;
 using SharedInterfaces;
 
 namespace DomainLoader
@@ -8,10 +9,7 @@ namespace DomainLoader
     {
         static string TypeLoaderAssemblyName
         {
-            get
-            {
-                return "SharedInterfaces";
-            }
+            get { return "SharedInterfaces"; }
         }
         void Run()
         {
@@ -20,6 +18,7 @@ namespace DomainLoader
             Log("Creating AppDomain");
             var loadedDomain = AppDomain.CreateDomain("Dynamically Loaded");
             Log("Loaded Domain: {0}", loadedDomain.Id);
+            ListLoadedAssembliesInCurrentDomain();
             // TODO: list loaded domains
             Log("Creating TypeLoader in Loaded Domain");
             var typeLoader =
@@ -27,11 +26,30 @@ namespace DomainLoader
                     loadedDomain.CreateInstance(TypeLoaderAssemblyName,
                         typeof (TypeLoader).FullName).Unwrap();
             Log("TypeLoader's AppDomain: {0}", typeLoader.AppDomainIdentifier);
-            Log("Using TypeLoader to get an instance of concrete type as an interface");
+            Log(
+                "Using TypeLoader to get an instance of concrete type as an interface");
             var remoteType =
-                typeLoader.Load("../../../SomeLibrary/bin/debug/SomeLibrary.dll", "SomeLibrary.AppDomainBoundaryCrosser");
+                typeLoader.Load(
+                                "../../../SomeLibrary/bin/debug/SomeLibrary.dll",
+                    "SomeLibrary.AppDomainBoundaryCrosser");
             Log("RemoteType's AppDomain: {0}", remoteType.AppDomainIdentifier);
-
+        }
+        void ListLoadedAssembliesInCurrentDomain()
+        {
+            ListLoadedAssembliesIn(AppDomain.CurrentDomain);
+        }
+        void ListLoadedAssembliesIn(AppDomain domain)
+        {
+            Log("AppDomain #{0}'s loaded assemblies:", domain.Id);
+            domain.GetAssemblies()
+                     .OrderBy(a => a.GetName().Name)
+                     .Select(a => a.GetName().Name)
+                     .UseIn(n => Log("\t{0}", n));
+            LogEndOfList();
+        }
+        void LogEndOfList()
+        {
+            Log("-------------------------------------------------------");
         }
         static void Main()
         {
