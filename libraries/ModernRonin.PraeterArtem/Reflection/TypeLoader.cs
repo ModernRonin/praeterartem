0323adb107aedf1399207dbf26a41cb5b6896643
Max <amodernronin@gmail.com>
2014-05-15 16:02:48 +0200

    removed caching - not needed, really

--- libraries/ModernRonin.PraeterArtem/Reflection/TypeLoader.cs
+++ libraries/ModernRonin.PraeterArtem/Reflection/TypeLoader.cs
@@ -7,9 +7,6 @@ namespace ModernRonin.PraeterArtem.Reflection
     [UsedImplicitly]
     sealed class TypeLoader : MarshalByRefObject
     {
-        static readonly ConcurrentDictionary<AppDomain, TypeLoader>
-            sAppDomainsToInstances =
-                new ConcurrentDictionary<AppDomain, TypeLoader>();
         /// <summary>
         ///     This MUST be an instance method, otherwise it will be called in the default AppDomain.
         /// </summary>
@@ -22,24 +19,9 @@ namespace ModernRonin.PraeterArtem.Reflection
         }
         static TypeLoader Create(AppDomain domain)
         {
-            return sAppDomainsToInstances.GetOrAdd(domain, DoCreate);
-        }
-        static TypeLoader DoCreate(AppDomain domain)
-        {
             var result =
                 TypeInAppDomainCreator.CreateTypeIn<TypeLoader>(domain);
-            domain.DomainUnload += OnDomainUnloading;
             return result;
-        }
-        static void OnDomainUnloading(object sender, EventArgs e)
-        {
-            Remove((AppDomain) sender);
-        }
-        static void Remove(AppDomain domain)
-        {
-            domain.DomainUnload -= OnDomainUnloading;
-            TypeLoader ignore;
-            sAppDomainsToInstances.TryRemove(domain, out ignore);
         }
         public static T CreateTypeInDomain<T>(AppDomain domain,
                                               string assemblyFilePath,
