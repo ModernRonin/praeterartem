80c1764fe5777ced2a64d5fd45c05484e213dbca
Max <amodernronin@gmail.com>
2014-05-17 20:17:37 +0200

    added StringExtensions and first method: .Repeat()

---
+++ libraries/ModernRonin.PraeterArtem/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using ModernRonin.PraeterArtem.Functional;
+
+namespace ModernRonin.PraeterArtem
+{
+    /// <summary>
+    ///     Contains extension methods for strings, basically stuff frequently needed and missing in the BCL.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        ///     Returns <paramref name="what" /> repeated <paramref name="count" /> times.
+        /// </summary>
+        /// <param name="what"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static string Repeat(this string what, int count)
+        {
+            var result = new StringBuilder();
+            count.TimesExecute(() => result.Append(what));
+            return result.ToString();
+        }
+    }
+}
