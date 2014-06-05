be9e8cc01030387c959348ddd29a60e3633cff37
Max <amodernronin@gmail.com>
2014-06-05 18:24:22 +0200

    forgotten files

---
+++ libraries/ModernRonin.PraeterArtem/Scheduling/IProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ModernRonin.PraeterArtem.Scheduling
+{
+    /// <summary>
+    /// Defines a processor which takes care of throttling of requests and
+    /// accounts for average lag from initialization of a request to its
+    /// fulfillment.
+    /// </summary>
+    /// <typeparam name="TWrappedProcessor"></typeparam>
+    public interface IProcessor<out TWrappedProcessor> :
+        IThrottlingConfigurable
+    {
+        /// <summary>The average time it took to execute a request.</summary>
+        TimeSpan AverageLag { get; }
+        /// <summary>
+        /// Can a request currently be executed, given throttling parameters
+        /// and latest load?
+        /// </summary>
+        bool CanCurrentlyExecuteRequest { get; }
+        /// <summary>How many requests count towards
+        /// <see cref="IThrottlingConfigurable.MaximumRequestCountPerDuration"/>
+        /// currently?</summary>
+        int CurrentRequestCountTowardsLimit { get; }
+        /// <summary>Execute a request.</summary>
+        /// <param name="request"></param>
+        void Execute(Action<TWrappedProcessor> request);
+    }
+}
---
+++ libraries/ModernRonin.PraeterArtem/Scheduling/ThrottlingScheduler.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using MoreLinq;
+
+namespace ModernRonin.PraeterArtem.Scheduling
+{
+    /// <summary>Implements <see cref="IScheduler{T}"/>.</summary>
+    /// <typeparam name="TWrappedProcessor"></typeparam>
+    public sealed class ThrottlingScheduler<TWrappedProcessor> :
+        AThrottlingConfigurable, IThrottlingConfigurable,
+        IScheduler<TWrappedProcessor>
+    {
+        readonly ConcurrentDictionary<IProcessor<TWrappedProcessor>, object>
+            mProcessors =
+                new ConcurrentDictionary
+                    <IProcessor<TWrappedProcessor>, object>();
+        readonly ConcurrentQueue<Action<TWrappedProcessor>> mRequests =
+            new ConcurrentQueue<Action<TWrappedProcessor>>();
+        public ThrottlingScheduler()
+        {
+            WeightOfLag = 1;
+            WeightOfLoad = 1;
+        }
+        /// <summary>
+        /// When choosing a processor, how much importance should the
+        /// processor's average lag have?
+        /// </summary>
+        public int WeightOfLag { get; set; }
+        /// <summary>
+        /// When choosing a processor, how much importance should the
+        /// processor's current number of requests towards the limit have?
+        /// </summary>
+        public int WeightOfLoad { get; set; }
+        bool HaveQueuedRequests
+        {
+            get { return mRequests.Any(); }
+        }
+        bool IsAnyProcessorAvailable
+        {
+            get
+            {
+                return mProcessors.Keys.Any(p => p.CanCurrentlyExecuteRequest);
+            }
+        }
+        public int QueueSize
+        {
+            get { return mRequests.Count; }
+        }
+        public int NumberOfProcessors
+        {
+            get { return mProcessors.Count; }
+        }
+        public void AddProcessor(IProcessor<TWrappedProcessor> processor)
+        {
+            processor.Duration = Duration;
+            processor.MaximumRequestCountPerDuration =
+                MaximumRequestCountPerDuration;
+            mProcessors.TryAdd(processor, null);
+        }
+        public void RemoveProcessor(IProcessor<TWrappedProcessor> processor)
+        {
+            object notNeeded;
+            mProcessors.TryRemove(processor, out notNeeded);
+        }
+        public void AddRequest(Action<TWrappedProcessor> request)
+        {
+            mRequests.Enqueue(request);
+        }
+        public void ProcessQueue()
+        {
+            while (IsAnyProcessorAvailable && HaveQueuedRequests)
+            {
+                Action<TWrappedProcessor> request;
+                mRequests.TryDequeue(out request);
+                var processor = GetBestProcessor();
+                processor.Execute(request);
+            }
+        }
+        IProcessor<TWrappedProcessor> GetBestProcessor()
+        {
+            return
+                mProcessors.Keys.Where(p => p.CanCurrentlyExecuteRequest)
+                           .MinBy(EvaluateProcessor);
+        }
+        object EvaluateProcessor(IProcessor<TWrappedProcessor> processor)
+        {
+            return processor.CurrentRequestCountTowardsLimit*WeightOfLoad +
+                   processor.AverageLag.TotalSeconds*WeightOfLag;
+        }
+    }
+}
